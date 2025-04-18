3685cc6a36176586274860187975d52426ede39a
HelloMonkey00 <kevinhome2007@gmail.com>
2025-04-19 02:25:38 +0800

    Update fetch_posts function in scrape.py to limit the number of posts fetched from the API to 5

--- scrape.py
+++ scrape.py
@@ -330,7 +330,7 @@ def fetch_posts(max_pages=3):
     for i in range(max_pages):
         try:
             # 构建URL
-            url = f"{BASE_URL}?limit=40"
+            url = f"{BASE_URL}?limit=5"
             if max_id:
                 url = f"{url}&max_id={max_id}"
             
